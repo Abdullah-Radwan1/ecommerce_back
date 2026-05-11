72980a170467a5131c161e3b04495470bf03ab59
abdullah <abdallahbeedo855@gmail.com>
2026-05-11 19:55:34 +0300

    using global pagination handler in order controller

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,5 +1,5 @@
 import Order from "../models/order.model.js";
-import Product from "../models/product.model.js";
+import Product from "../models/Product.model.js";
 import Cart from "../models/cart.model.js";
 import { catchAsync } from "../utilities/catchAsync.ut.js";
 import { AppError } from "../utilities/appError.ut.js";
